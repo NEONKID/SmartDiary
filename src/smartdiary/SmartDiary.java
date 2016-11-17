b7f5131c9834e765edef0bca07a1dad643de1904
Neon K.I.D <clax1412@gmail.com>
2016-11-17 11:07:10 +0900

    Alert Code 추가,,

--- src/smartdiary/SmartDiary.java
+++ src/smartdiary/SmartDiary.java
@@ -9,6 +9,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 
 /**
@@ -17,10 +18,13 @@ import javafx.stage.Stage;
  */
 public class SmartDiary extends Application {
     private static Stage stage;
+    private String username = System.getProperty("user.name");
+    private Alert alert;
 
     public static Stage getStage() {
         return stage;
     }
+
     @Override
     public void start(Stage LoginStage) throws Exception {
         Parent loginView = FXMLLoader.load(getClass().getResource("Login.fxml"));
@@ -28,8 +32,10 @@ public class SmartDiary extends Application {
         stage = LoginStage;
         stage.setTitle("Welcome to SmartDiary !");
         stage.setScene(new Scene(loginView));
-
         stage.show();
+
+        System.out.println(System.getProperty("os.name"));
+        System.out.println(System.getProperty("user.name"));
     }
 
     /**
@@ -37,6 +43,21 @@ public class SmartDiary extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void checkOS(int code) {
+        switch(code) {
+            case 0:
+                break;
+            case 1:
+                break;
+            default:
+                alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("프로그램 실행 오류");
+                alert.setHeaderText("");
+                alert.setContentText("");
+                break;
+        }
     }
 
 }
